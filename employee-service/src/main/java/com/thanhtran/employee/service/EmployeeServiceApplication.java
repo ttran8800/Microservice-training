5609c247f946b41b7bac92707fa445da51f4e6e1
Thanh <ttran8800@gmail.com>
2024-04-03 10:51:31 -0500

    updated main classes to use annotation, yml file does not need configs for eureka anymore

--- employee-service/src/main/java/com/thanhtran/employee/service/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/thanhtran/employee/service/EmployeeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
